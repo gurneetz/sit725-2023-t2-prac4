f004d26a3e3433cc71d4a721d3f9acd23682df24
gurneetz <139436124+gurneetz@users.noreply.github.com>
2023-08-08 21:36:58 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,63 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require("express")
+var app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://gurneetsin:Noneedofit@cluster0.9xakcg6.mongodb.net/?retryWrites=true&w=majority";
+var port = process.env.port || 3000;
+let collection;
 
-app.listen(port, () => {
-  console.log("App listening on port: " + port);
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.get('/api/cats', (req,res) => {
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200, data:result, message:'get all cats successful'});
+        }
+    });
+});
+
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
+
+function postCat(cat,callback) {
+    collection.insertOne(cat,callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
+
+app.listen(port, ()=>{
+    console.log('express server started');
+    runDBConnection();
 });
